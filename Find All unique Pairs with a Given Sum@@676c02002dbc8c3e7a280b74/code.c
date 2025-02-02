fededf658753b5bd41e6d14b15233c4025e66925
Shivansh-012 <shivansh1207@gmail.com>
2025-02-02 16:24:48 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d ",&arr[i]);
-    }
-    int t;
-    scanf("%d",&t);
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i] + arr[j] == t){
-                printf("%d %d\n",arr[i],arr[j]);
-            }
+// Comparator function for qsort
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+void findPairs(int arr[], int n, int target) {
+    qsort(arr, n, sizeof(int), compare);  // Sorting the array: O(n log n)
+
+    int left = 0, right = n - 1;
+    
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == target) {
+            printf("%d %d\n", arr[left], arr[right]);
+            left++;  // Move left pointer forward
+            right--; // Move right pointer backward to avoid duplicates
+        } 
+        else if (sum < target) {
+            left++;  // Increase sum by moving left pointer
+        } 
+        else {
+            right--; // Decrease sum by moving right pointer
         }
     }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int t;
+    scanf("%d", &t);
+
+    findPairs(arr, n, t);
+
     return 0;
 }
