ba89d0248b569e88021dc7d9118d3e3a64fb9248
Shivansh-012 <shivansh1207@gmail.com>
2025-02-08 18:46:22 +0530

    Solution to Find Student with Minimum Marks

--- Find Student with Minimum Marks@@67877d61dc107486ad5c2749/code.c
+++ Find Student with Minimum Marks@@67877d61dc107486ad5c2749/code.c
@@ -16,7 +16,7 @@ int main(){
     float min = 0.0;
     for(int i=0;i<n;i++){
         if(s[i].m < min){
-            min = s[i].min
+            min = s[i].min;
         }
     }
     for(int i=0;i<n;i++){
