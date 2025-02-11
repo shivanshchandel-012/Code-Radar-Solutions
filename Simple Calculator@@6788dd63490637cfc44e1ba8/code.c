be6905e1b422515660716ce18cbf4897d9962be0
Shivansh-012 <shivansh1207@gmail.com>
2025-02-11 12:53:41 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,11 +5,11 @@ int main(){
     char c;
     scanf("%lf %lf %c",&a,&b,&c);
     if(c == '+'){
-        printf("%lf",a+b);
+        printf("%.0lf",a+b);
     }else if(c == '*'){
-        printf("%lf",a*b);
+        printf("%.0lf",a*b);
     }else if(c == '/'){
-        printf("%lf",a/b);
+        printf("%.0lf",a/b);
     }else if(c == '-'){
         printf("%.0lf",a-b);
     }
