8dce5203fab75372b94de0615cb884cb7813dc33
Shivansh Chandel <shivansh1207@gmail.com>
2025-03-11 20:47:36 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -4,10 +4,10 @@ void s(char str[]){
     int len = strlen(str);
     for(int i=0;str[i]<len-1;i++){
         for(int j=i+1;j<len;j++){
-            if(str[i] < str[j]){
-                char temp = str[j];
-                str[j] = str[i];
-                str[i] = temp;
+            if(str[i] > str[j]){
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
             }
         }
     }
