cec985a071caf5c5c12a96816984bf95ae2473d6
Shivansh Chandel <shivansh1207@gmail.com>
2025-03-11 20:11:41 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
 
-int main(){
+int main() {
     char str[100];
-    int freq[256] = {};
-    int i=0;
-    while(scanf("%c",&str[i]) != '\n' && str[i] != '\n'){
+    int freq[256] = {0}; 
+    int i = 0;
+    while (scanf("%c", &str[i]) && str[i] != '\n') {
         i++;
     }
-    i = '\0';
-    for(int j=0;str[j]!=EOF;j++){
+    str[i] = '\0'; 
+    for (int j = 0; str[j] != '\0'; j++) {
         freq[str[j]]++;
     }
     char mf = str[0];
-    int mc = str[0];
-    for(int j=1;str[j]!=EOF;j++){
-        if(freq[str[j]] > mc){
+    int mc = freq[str[0]];
+
+    for (int j = 1; str[j] != '\0'; j++) {
+        if (freq[str[j]] > mc) {
             mc = freq[str[j]];
             mf = str[j];
         }
-        else if(mc == freq[str[j]]){
-            if(str[j] < mf) mf = str[j];
+        else if (freq[str[j]] == mc) {
+            if (str[j] < mf) {  
+                mf = str[j];
+            }
         }
     }
+
     printf("%c",mf);
     return 0;
 }
