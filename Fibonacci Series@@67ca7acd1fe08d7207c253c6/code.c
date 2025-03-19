a36b25e596c4d7bab4066fd4d80b6d8083b60bc4
Shivansh Chandel <shivansh1207@gmail.com>
2025-03-19 13:40:44 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,7 +1,7 @@
 int fibo(int n){
     if(n==0) return 0;
     else if(n==1) return 1;
-    else fibo(n-1)+fibo(n-1);
+    else return fibo(n-1)+fibo(n-1);
 }
 void fibonacciSeries(int n){
     for(int i=0;i<n;i++){
